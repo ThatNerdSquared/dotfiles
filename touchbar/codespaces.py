2ba7c7b5d0121cb184cd25f6f59ac70a2591cdc9
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2021-09-02 13:30:49 -0700

    Added codespaces touchbar shortcut

---
+++ touchbar/codespaces.py
@@ -0,0 +1,59 @@
+import os
+import tkinter as tk
+
+def activate(space):
+    path = space[0]
+    os.system('osascript -e \'tell application \"iTerm\" to create window with profile \"Default\"\'')
+    os.system(f'osascript -e \'tell application \"System Events\" to keystroke \"/opt/homebrew/bin/nvim -S /Users/nathanyeung/OneDrive/neovim-sessions/{path}.vim\"\'')
+    os.system('osascript -e \'tell application \"System Events\" to keystroke return\'')
+    window.destroy()
+    return
+
+
+workspaces = [
+	['horace', '/Users/nathanyeung/h4ck3r/koa/horace'],
+	['hyperfocus-app', '/Users/nathanyeung/h4ck3r/hyperfocus-app'],
+	['scripts', '/Users/nathanyeung/dotfiles/scripts'],
+	['xmaets-bot', '/Users/nathanyeung/h4ck3r/xmaets-bot'],
+	['widgets', '/Users/nathanyeung/Library/Application Support/Übersicht/widgets'],
+	['tutorial-app', '/Users/nathanyeung/h4ck3r/tutorial-app'],
+	['advent-of-code-2020', '/Users/nathanyeung/h4ck3r/advent-of-code-2020'],
+	['pom-bot', '/Users/nathanyeung/h4ck3r/pom-bot'],
+	['expressjs-tutorial', '/Users/nathanyeung/h4ck3r/expressjs-tutorial'],
+	['public-code', '/Users/nathanyeung/h4ck3r/public-code'],
+	['steamx-git', '/Users/nathanyeung/OneDrive/Documents/steamx-git'],
+	['zeus-bot', '/Users/nathanyeung/h4ck3r/zeus-bot']
+]
+
+window = tk.Tk()
+window.title('Codespace Selection')
+label = tk.Label(text='Select codespace:')
+label.pack()
+
+buttons = {}
+for space in workspaces:
+    def action(x = space):
+        return activate(x)
+    buttons[space[0]] = tk.Button(text = space[0], command = action)
+    buttons[space[0]].pack()
+
+window.mainloop()
+
+'''
+for space in workspaces:
+    layout.append([sg.Checkbox(space[1])])
+layout.append([sg.Submit(), sg.Cancel()])
+
+value = form.LayoutAndRead(layout)
+
+folder = workspaces[value]
+
+commands = [
+'tell application \"iTerm\"'
+'activate'
+'try'
+'create window with profile "Default" command nvim {folder[1]}'
+'end try'
+'end tell'
+]
+'''
